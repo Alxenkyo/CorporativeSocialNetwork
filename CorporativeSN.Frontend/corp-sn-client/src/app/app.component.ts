04238b38bb8e216aab34318e0129114c3fb46c08
DESKTOP-QTSCE5H\USER <yafantom11@gmail.com>
2021-04-17 17:29:51 +0300

    Commit

--- CorporativeSN.Frontend/corp-sn-client/src/app/app.component.ts
+++ CorporativeSN.Frontend/corp-sn-client/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+//import {UserService} from './Services/user.service'
+//import {LoginService} from './Services/login.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'corp-sn-client';
+  //currentUser: User;
+    static isAdmin: boolean = false;
+    //private _userService: UserService; 
+    constructor(
+        private router: Router,
+       
+    ) {
+        
+    }
+    ngOnInit(){
+        this.isLogin();
+    }
+    isLogin(){
+        var value = localStorage.getItem('Bearer')
+          if(value!=null){ 
+            this.router.navigate(['/chats'])
+          }else{ 
+            this.router.navigate(['/login'])
+          }
+      }
+    logout() {
+        this.router.navigate(['/login']);
+    }
 }
