e503f71299b7e4265651d5fe65282deeea6a17ad
DESKTOP-QTSCE5H\USER <yafantom11@gmail.com>
2021-04-17 16:46:13 +0300

    Commit

--- CorporativeSN/CorporativeSN.Logic/Models/EventDto.cs
+++ CorporativeSN/CorporativeSN.Logic/Models/EventDto.cs
@@ -7,11 +7,13 @@ namespace CorporativeSN.Logic.Models
 {
     public class EventDTO
     {
-        public int Id { get; set; }
         [Required]
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required]
         public DateTime Date { get; set; }
+        [Required]
         public int CreatorId { get; set; }
         //public User Creator { get; set; }
         public IEnumerable<UserDTO> Users { get; set; }
--- CorporativeSN/CorporativeSN.Logic/Models/UserDto.cs
+++ CorporativeSN/CorporativeSN.Logic/Models/UserDto.cs
@@ -8,14 +8,12 @@ namespace CorporativeSN.Logic.Models
 {
     public class UserDTO
     {
-        [Required]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public int UserTypeId { get; set; }
-        [Required]
         public int DepartmentId { get; set; }
-        public IEnumerable<EventDTO> CreatedEvents { get; set; }
+        //public IEnumerable<EventDTO> CreatedEvents { get; set; }
     }
 }
